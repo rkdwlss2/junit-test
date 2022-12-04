62d9466c5481eeb8003fe148d5a08ea70b1389fc
rkdwlss2 <rkdwlss2@gmail.com>
2022-12-04 22:08:00 +0900

    repository 테스트 환경 세팅 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,5 +1,25 @@
 package site.metacoding.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest // 디비와 관련된 로직만 메모리에 띄움
 public class BookRepositoryTest {
-    
+
+    @Autowired
+    private BookRepository bookRepository;
+    // 1. 책 등록
+    @Test
+    public void 책등록_test(){
+        System.out.println("책등록_test 실행");
+    }
+
+    // 2. 책 목록 보기
+
+    // 3. 책 한건 보기
+
+    // 4. 책 수정
+
+    // 6. 책 삭제
 }
